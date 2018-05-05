5b932e1ac1059f1fbc8ceebcfb4026fc86627257
PrasunPK <prasun_1993@rediffmail.com>
2018-05-05 14:44:00 +0530

    Added identity section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import TopAndLeft from "./TopAndLeft";
 import MainContainer from "./container";
+import LeftNavigation from "./LeftNavigation";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div>
+                <div className="left-section">
+                    <LeftNavigation/>
+                </div>
+                <div className="right-section">
                     <TopAndLeft/>
                     <MainContainer/>
                 </div>
---
+++ src/LeftNavigation.js
@@ -0,0 +1,28 @@
+import Card from "material-ui/es/Card/Card";
+import CardContent from "material-ui/es/Card/CardContent";
+import React from "react";
+import Typography from "material-ui/es/Typography/Typography";
+import CardActions from "material-ui/es/Card/CardActions";
+import Avatar from "material-ui/es/Avatar/Avatar";
+
+const LeftNavigation = () => {
+    return (
+        <Card className="left-menu-section">
+            <CardContent>
+                <Avatar className="large-avatar">
+                    Magneto
+                </Avatar>
+                <div className="menu-heading">
+                    <Typography variant="headline" component="h1">
+                        Pairing Matrix
+                    </Typography>
+                    <hr/>
+                </div>
+            </CardContent>
+            <CardActions>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default LeftNavigation;

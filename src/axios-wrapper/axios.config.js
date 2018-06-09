cdd909b828a7dc953eebc0eefbb50828a0a3065a
PrasunPK <prasun_1993@rediffmail.com>
2018-06-09 13:41:03 +0530

    hard coding service url

--- src/axios-wrapper/axios.config.js
+++ src/axios-wrapper/axios.config.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8080/'
-        : 'https://dailysummary.herokuapp.com/'
+    baseURL: 'https://dailysummary.herokuapp.com/'
 });

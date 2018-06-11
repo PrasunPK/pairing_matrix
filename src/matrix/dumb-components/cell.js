f591bbc9070f4e956b7adab26f0287ed96cd5b1e
PrasunPK <prasun_1993@rediffmail.com>
2018-06-11 21:43:22 +0530

    gray out unused cells in matrix

--- src/matrix/dumb-components/cell.js
+++ src/matrix/dumb-components/cell.js
@@ -2,31 +2,41 @@ import React from "react";
 import {Grid, IconButton, Paper} from "@material-ui/core/es/index";
 
 const Cell = ({rowMember, member, handleAddClick, handleMinusClick, count, pIndex, cIndex}) => {
-    return (<Grid item>
-            <Paper style={{paddingTop: 20, paddingBottom: 9, height: 33, width: 80}}>
-                {cIndex <= pIndex ?
-                    <div>
-                        <div className="pair-value" id={`${rowMember.memberId}_${member.memberId}_value`}>
-                            {count > 0 ? count : 0}
+    return (
+        <Grid item>
+            {
+                cIndex <= pIndex ?
+                    <Paper style={{
+                        paddingTop: 20,
+                        paddingBottom: 9,
+                        height: 33, width: 80
+                    }}>
+                        <div>
+                            <div className="pair-value" id={`${rowMember.memberId}_${member.memberId}_value`}>
+                                {count > 0 ? count : 0}
+                            </div>
+                            <div className="fab-action-icons">
+                                <IconButton className="fab-action-button add-button"
+                                            id={`${rowMember.memberId}_${member.memberId}_add`}
+                                            onClick={handleAddClick}>
+                                    +
+                                </IconButton>
+                                <IconButton className="fab-action-button minus-button"
+                                            id={`${rowMember.memberId}_${member.memberId}_minus`}
+                                            onClick={handleMinusClick}>
+                                    -
+                                </IconButton>
+                            </div>
                         </div>
-                        <div className="fab-action-icons">
-                            <IconButton className="fab-action-button add-button"
-                                        id={`${rowMember.memberId}_${member.memberId}_add`}
-                                        onClick={handleAddClick}>
-                                +
-                            </IconButton>
-                            <IconButton className="fab-action-button minus-button"
-                                        id={`${rowMember.memberId}_${member.memberId}_minus`}
-                                        onClick={handleMinusClick}>
-                                -
-                            </IconButton>
-                        </div>
-                    </div> :
-                    null
-                }
-            </Paper>
-        </Grid>
-    )
+                    </Paper> :
+                    <Paper style={{
+                        paddingTop: 20,
+                        paddingBottom: 9,
+                        height: 33, width: 80,
+                        backgroundColor: "#D7D7D7"
+                    }}/>
+            }
+        </Grid>)
 };
 
 export default Cell;
